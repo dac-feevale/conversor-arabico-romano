488160b7c994767205914291f491727d6b410505
Gabriel Hoff <gabrielhof@gmail.com>
2015-06-10 15:50:15 -0300

    Implementação da conversão de cada casa decimal para números romanos

--- src/br/feevale/conversor/ConversorArabicoParaRomano.java
+++ src/br/feevale/conversor/ConversorArabicoParaRomano.java
@@ -3,7 +3,60 @@ package br.feevale.conversor;
 public class ConversorArabicoParaRomano {
 
 	public String converte(String arabico) {
-		return "I";
+		if (arabico.length() == 1) {
+			return converteUltimaCasa(arabico.charAt(0));
+		} else if (arabico.length() == 2) {
+			return converteSegundaCasa(arabico.charAt(0)) + converteUltimaCasa(arabico.charAt(1));
+		} else if (arabico.length() == 3) {
+			return converteTerceiraCasa(arabico.charAt(0)) + converteSegundaCasa(arabico.charAt(1)) + converteUltimaCasa(arabico.charAt(2));
+		}
+		
+		return null;
+	}
+
+	protected String converteUltimaCasa(char digito) {
+		switch (digito) {
+		case '1': return "I";
+		case '2': return "II";
+		case '3': return "III";
+		case '4': return "IV";
+		case '5': return "V";
+		case '6': return "VI";
+		case '7': return "VII";
+		case '8': return "VIII";
+		case '9': return "IX";
+		default: return "";
+		}
+	}
+	
+	private String converteSegundaCasa(char digito) {
+		switch (digito) {
+		case '1': return "X";
+		case '2': return "XX";
+		case '3': return "XXX";
+		case '4': return "XL";
+		case '5': return "L";
+		case '6': return "LX";
+		case '7': return "LXX";
+		case '8': return "LXXX";
+		case '9': return "XC";
+		default: return "";
+		}
+	}
+	
+	private String converteTerceiraCasa(char digito) {
+		switch (digito) {
+		case '1': return "C";
+		case '2': return "CC";
+		case '3': return "CCC";
+		case '4': return "CD";
+		case '5': return "D";
+		case '6': return "DC";
+		case '7': return "DCC";
+		case '8': return "DCCC";
+		case '9': return "DM";
+		default: return "";
+		}
 	}
 	
 }
--- test/br/feevale/conversor/ConversorArabicoParaRomanoTest.java
+++ test/br/feevale/conversor/ConversorArabicoParaRomanoTest.java
@@ -13,4 +13,124 @@ public class ConversorArabicoParaRomanoTest {
 		Assert.assertEquals("I", resultado);
 	}
 	
+	@Test
+	public void converteNumero4ParaIVTest() {
+		ConversorArabicoParaRomano conversor = new ConversorArabicoParaRomano();
+		String resultado = conversor.converte("4");
+		
+		Assert.assertEquals("IV", resultado);
+	}
+	
+	@Test
+	public void converteNumero5ParaVTest() {
+		ConversorArabicoParaRomano conversor = new ConversorArabicoParaRomano();
+		String resultado = conversor.converte("5");
+		
+		Assert.assertEquals("V", resultado);
+	}
+	
+	@Test
+	public void converteNumero9ParaIXTest() {
+		ConversorArabicoParaRomano conversor = new ConversorArabicoParaRomano();
+		String resultado = conversor.converte("9");
+		
+		Assert.assertEquals("IX", resultado);
+	}
+	
+	@Test
+	public void converteNumero10ParaXTest() {
+		ConversorArabicoParaRomano conversor = new ConversorArabicoParaRomano();
+		String resultado = conversor.converte("10");
+		
+		Assert.assertEquals("X", resultado);
+	}
+	
+	@Test
+	public void converteNumero11ParaXITest() {
+		ConversorArabicoParaRomano conversor = new ConversorArabicoParaRomano();
+		String resultado = conversor.converte("11");
+		
+		Assert.assertEquals("XI", resultado);
+	}
+	
+	@Test
+	public void converteNumero14ParaXIVTest() {
+		ConversorArabicoParaRomano conversor = new ConversorArabicoParaRomano();
+		String resultado = conversor.converte("14");
+		
+		Assert.assertEquals("XIV", resultado);
+	}
+	
+	@Test
+	public void converteNumero15ParaXVTest() {
+		ConversorArabicoParaRomano conversor = new ConversorArabicoParaRomano();
+		String resultado = conversor.converte("15");
+		
+		Assert.assertEquals("XV", resultado);
+	}
+	
+	@Test
+	public void converteNumero19ParaXIXTest() {
+		ConversorArabicoParaRomano conversor = new ConversorArabicoParaRomano();
+		String resultado = conversor.converte("19");
+		
+		Assert.assertEquals("XIX", resultado);
+	}
+	
+	@Test
+	public void converteNumero20ParaXXTest() {
+		ConversorArabicoParaRomano conversor = new ConversorArabicoParaRomano();
+		String resultado = conversor.converte("20");
+		
+		Assert.assertEquals("XX", resultado);
+	}
+	
+	@Test
+	public void converteNumero40ParaXLTest() {
+		ConversorArabicoParaRomano conversor = new ConversorArabicoParaRomano();
+		String resultado = conversor.converte("40");
+		
+		Assert.assertEquals("XL", resultado);
+	}
+	
+	@Test
+	public void converteNumero44ParaXLIVTest() {
+		ConversorArabicoParaRomano conversor = new ConversorArabicoParaRomano();
+		String resultado = conversor.converte("44");
+		
+		Assert.assertEquals("XLIV", resultado);
+	}
+	
+	@Test
+	public void converteNumero50ParaLTest() {
+		ConversorArabicoParaRomano conversor = new ConversorArabicoParaRomano();
+		String resultado = conversor.converte("50");
+		
+		Assert.assertEquals("L", resultado);
+	}
+	
+	@Test
+	public void converteNumero60ParaLXTest() {
+		ConversorArabicoParaRomano conversor = new ConversorArabicoParaRomano();
+		String resultado = conversor.converte("60");
+		
+		Assert.assertEquals("LX", resultado);
+	}
+	
+	@Test
+	public void converteNumero90ParaXCTest() {
+		ConversorArabicoParaRomano conversor = new ConversorArabicoParaRomano();
+		String resultado = conversor.converte("90");
+		
+		Assert.assertEquals("XC", resultado);
+	}
+	
+	@Test
+	public void converteNumero100ParaXTest() {
+		ConversorArabicoParaRomano conversor = new ConversorArabicoParaRomano();
+		String resultado = conversor.converte("100");
+		
+		Assert.assertEquals("C", resultado);
+	}
+	
 }
